5690bf8dbd4d2fece2bf444e0281b0f9c93de61d
Felipe Ribeiro <piporibeiro@gmail.com>
2022-02-23 21:16:01 -0300

    feat: pass resolvers and imports

--- src/modules/students/students.module.ts
+++ src/modules/students/students.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
-import { StudentsService } from './students.service';
-import { StudentsResolver } from './students.resolver';
+import { NestjsQueryGraphQLModule } from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
+import { Student } from './entities/student.entity';
+import { StudentDTO } from './dto/student.dto';
+import { CreateStudentInput } from './dto/create-student.input';
+import { UpdateStudentInput } from './dto/update-student.input';
 
 @Module({
-  providers: [StudentsResolver, StudentsService]
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([Student])],
+      resolvers: [
+        {
+          DTOClass: StudentDTO,
+          EntityClass: Student,
+          CreateDTOClass: CreateStudentInput,
+          UpdateDTOClass: UpdateStudentInput,
+          enableTotalCount: true,
+        },
+      ],
+    }),
+  ],
+  providers: [],
 })
 export class StudentsModule {}
